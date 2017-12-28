d9723e835bc9e63e5314ae1666df459589260efc
Ricky Tham <rickytham123@gmail.com>
2017-12-27 21:07:23 -0800

    Test

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,17 @@
 var myHeading = document.query.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+function openCity(cityName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(cityName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+
+}
