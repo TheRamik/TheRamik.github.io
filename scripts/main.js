d36b24bc466ca5845a81cfe735645c597d16f9c4
Ricky Tham <rickytham123@gmail.com>
2017-12-27 23:02:35 -0800

    script

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,9 @@
-function openCity(cityName,elmnt,color) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-    document.getElementById(cityName).style.display = "block";
-    elmnt.style.backgroundColor = color;
 
+function changeNavi() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
-
-document.getElementById("defaultOpen").click();
-
-var myHeading = document.query.querySelector('h1');
-myHeading.textContent = 'Hello world!';
