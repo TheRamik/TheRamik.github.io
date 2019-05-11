81292de04b3ea6868f8c5815e1ba16f665f04053
Ricky Tham <rickytham123@gmail.com>
2019-05-11 14:32:20 -0700

    Performed some styling in the about me page
    Created about-me.module.scss for styling.

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -4,14 +4,17 @@ import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import aboutMeStyles from './about-me.module.scss'
 
 export const query = graphql`
   query {
     placeholderImage: file(relativePath: { eq: "images/headshot-suit.jpg" }) {
       childImageSharp {
-        fixed(width: 125, height: 125) {
-          ...GatsbyImageSharpFixed
-        }
+        fluid(maxWidth: 700) {
+          ...GatsbyImageSharpFluid
+        },
+        
+        
       }
     }
   }
@@ -22,10 +25,12 @@ const AboutMe = ( {data} ) => (
     <SEO title="About Me" />
     <h1>About Me</h1>
     <hr></hr>
-    <Img fixed={data.placeholderImage.childImageSharp.fixed} />
-    <p>A recent graduate (2018) from University of California, Irvine (UCI) with a degree in Computer Science. 
+    <Img className={aboutMeStyles.introImg}
+      fluid={data.placeholderImage.childImageSharp.fluid} 
+    />
+    <p className={aboutMeStyles.intro}>
+      A recent graduate (2018) from University of California, Irvine (UCI) with a degree in Computer Science. 
       I am currently a Systems Engineer in the Orange County area at a company called Unisys.
-
     </p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
